6018baad3a5746ed950e97097e69b49ffbc4c05b
신관규 <soundbar91@gmail.com>
2025-04-12 19:40:06 +0900

    feat: 비밀번호 예외 클래스 추가

---
+++ src/main/java/com/hlp/api/domain/user/exception/NotMatchPasswordException.java
@@ -0,0 +1,9 @@
+package com.hlp.api.domain.user.exception;
+
+import com.hlp.api.common.exception.custom.AuthenticationException;
+
+public class NotMatchPasswordException extends AuthenticationException {
+    public NotMatchPasswordException(String message, String detail) {
+        super(message, detail);
+    }
+}
