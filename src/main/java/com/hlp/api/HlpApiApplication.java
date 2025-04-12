73bb7d1cfb76c349c8687630897732b7deac6482
신관규 <soundbar91@gmail.com>
2025-04-12 16:42:56 +0900

    init commit

---
+++ src/main/java/com/hlp/api/HlpApiApplication.java
@@ -0,0 +1,13 @@
+package com.hlp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HlpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hlp/api/HlpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
