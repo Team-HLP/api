44ee121f55d04c2bb89950f041c94951978e7e7a
신관규 <soundbar91@gmail.com>
2025-04-13 13:22:03 +0900

    feat: Admin 엔티티 클래스 추가

---
+++ src/main/java/com/hlp/api/admin/model/Admin.java
@@ -0,0 +1,60 @@
+package com.hlp.api.admin.model;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "admins")
+@NoArgsConstructor(access = PROTECTED)
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "login_id", nullable = false)
+    private String loginId;
+
+    @NotNull
+    @Column(name = "password", nullable = false, columnDefinition = "MEDIUMTEXT")
+    private String password;
+
+    @NotNull
+    @Size(max = 20)
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Size(max = 20)
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Builder
+    private Admin(
+        Integer id,
+        String loginId,
+        String password,
+        String name,
+        String phoneNumber
+    ) {
+        this.id = id;
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
