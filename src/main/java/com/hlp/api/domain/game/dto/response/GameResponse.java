51c0e7b388049ff862015e4f44bea3dc6318bc09
신관규 <soundbar91@gmail.com>
2025-04-14 23:09:02 +0900

    fix: dto 어노테이션 설정 수정

--- src/main/java/com/hlp/api/domain/game/dto/response/GameResponse.java
+++ src/main/java/com/hlp/api/domain/game/dto/response/GameResponse.java
@@ -13,19 +13,19 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonNaming(value = SnakeCaseStrategy.class)
 public record GameResponse(
-    @Schema(name = "게임 id", example = "1", requiredMode = REQUIRED)
+    @Schema(description = "게임 id", example = "1", requiredMode = REQUIRED)
     Integer id,
 
-    @Schema(name = "결과", example = "성공", requiredMode = REQUIRED)
+    @Schema(description = "결과", example = "성공", requiredMode = REQUIRED)
     String result,
 
-    @Schema(name = "점수", example = "1234", requiredMode = REQUIRED)
+    @Schema(description = "점수", example = "1234", requiredMode = REQUIRED)
     Integer score,
 
-    @Schema(name = "체력", example = "55", requiredMode = REQUIRED)
+    @Schema(description = "체력", example = "55", requiredMode = REQUIRED)
     Integer hp,
 
-    @Schema(name = "운석 파괴 수", example = "54", requiredMode = REQUIRED)
+    @Schema(description = "운석 파괴 수", example = "54", requiredMode = REQUIRED)
     Integer meteoriteBrokenCount,
 
     @Schema(description = "게임 생성일", example = "2025.04.12 14:12")
