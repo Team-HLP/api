779a0d65e322a976ca940a6620c79c8a56b66ba7
신관규 <soundbar91@gmail.com>
2025-04-25 12:54:04 +0900

    feat: 생체 데이터 업로드 추가

--- src/main/java/com/hlp/api/domain/game/controller/GameApi.java
+++ src/main/java/com/hlp/api/domain/game/controller/GameApi.java
@@ -1,11 +1,16 @@
 package com.hlp.api.domain.game.controller;
 
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hlp.api.common.auth.user.UserAuth;
 import com.hlp.api.domain.game.dto.request.GameCreateRequest;
@@ -33,7 +38,9 @@ public interface GameApi {
     @Operation(summary = "게임 정보 생성")
     @PostMapping("/game")
     ResponseEntity<GameResponse> createGame(
-        @RequestBody @Valid GameCreateRequest request,
+        @RequestPart("request") @Valid GameCreateRequest request,
+        @RequestPart("eeg_data_file") MultipartFile eegDataFile,
+        @RequestPart("eye_data_file") MultipartFile eyeDataFile,
         @UserAuth Integer userId
     );
 
--- src/main/java/com/hlp/api/domain/game/service/GameService.java
+++ src/main/java/com/hlp/api/domain/game/service/GameService.java
@@ -1,13 +1,19 @@
 package com.hlp.api.domain.game.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.hlp.api.common.config.FileStorageProperties;
 import com.hlp.api.domain.game.dto.request.GameCreateRequest;
 import com.hlp.api.domain.game.dto.response.GameResponse;
+import com.hlp.api.domain.game.exception.DataFileSaveException;
 import com.hlp.api.domain.game.model.Game;
 import com.hlp.api.domain.game.repository.GameRepository;
 import com.hlp.api.domain.user.model.User;
@@ -22,17 +28,39 @@ public class GameService {
 
     private final GameRepository gameRepository;
     private final UserRepository userRepository;
+    private final FileStorageProperties fileStorageProperties;
 
     @Transactional
-    public GameResponse createGame(GameCreateRequest request, Integer userId) {
+    public GameResponse createGame(
+        GameCreateRequest request, MultipartFile eegDatafile, MultipartFile eyeDatafile, Integer userId
+    ) {
         User user = userRepository.getById(userId);
-        Game game = request.toEntity(user);
-        return GameResponse.of(gameRepository.save(game));
+        Game game = gameRepository.save(request.toEntity(user));
+
+        String path = String.format(fileStorageProperties.path(), System.getProperty("user.dir"), user.getId(), game.getId());
+
+        try {
+            File directory = new File(path);
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            File eegFile = new File(directory, Objects.requireNonNull(eegDatafile.getOriginalFilename()));
+            File eyeFile = new File(directory, Objects.requireNonNull(eyeDatafile.getOriginalFilename()));
+
+            eegDatafile.transferTo(eegFile);
+            eyeDatafile.transferTo(eyeFile);
+
+        } catch (IOException e) {
+            throw new DataFileSaveException("생체 데이터 저장 과정에서 오류가 발생했습니다");
+        }
+
+        return GameResponse.of(game);
     }
 
     public List<GameResponse> getGames(Integer userId) {
         User user = userRepository.getById(userId);
-        List<Game> games = gameRepository.findAllByUserId(userId);
+        List<Game> games = gameRepository.findAllByUserId(user.getId());
         return games.stream().map(GameResponse::of).collect(Collectors.toList());
     }
 }
