dd02c6dc708fcf90bfe017efde56e3cdf3c50595
신관규 <soundbar91@gmail.com>
2025-05-19 23:17:21 +0900

    fix: 게임 명 변경

--- src/main/java/com/hlp/api/domain/game/model/GameCategory.java
+++ src/main/java/com/hlp/api/domain/game/model/GameCategory.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum GameCategory {
-    METEORITE_DESTRUCTION("운석 부수기"),
-    CATCH_MOLE("두더지 잡기"),
+    METEORITE_DESTRUCTION("쾅! 운석 요격전"),
+    CATCH_MOLE("외계인을 물리쳐라"),
     ;
 
     private final String displayName;
